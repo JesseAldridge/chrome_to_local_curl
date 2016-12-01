b3c94e7bf52656cddc4c404b45e00edfca63e04b
Jesse Aldridge <JesseAldridge@gmail.com>
2016-11-30 17:22:47 -0800

    add quotes

--- chrome_to_local_curl.py
+++ chrome_to_local_curl.py
@@ -20,13 +20,16 @@ local_domain_name = 'localhost:5001'
 token_iter = iter(tokens)
 filtered_tokens = []
 for token in token_iter:
+    print 'token:', token
+
     if token.startswith('http'):
         domain_match = re.search(r'(https?\://.+?)/', token)
         domain_str = domain_match.group(1)
         host_name = re.search(r'https?\://(.+?)$', domain_str).group(1)
         filtered_tokens.append("'{}'".format(token.replace(domain_str, local_domain_name)))
         creds = host_to_creds[host_name]
-        filtered_tokens += ['--user', ':'.join((creds['username'], creds['password']))]
+        user = '"{}"'.format(':'.join((creds['username'], creds['password'])))
+        filtered_tokens += ['--user', user]
     elif token in {'--data-binary', '--data', '-d'}:
         next_token = token_iter.next()
         post_data = json.loads(next_token)
